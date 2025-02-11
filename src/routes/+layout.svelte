26d517577d643ee08045153eb62ebd882673d6f3
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-11 23:39:00 +0900

    add layout file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+let { children } = $props()
+</script>
+
+<main class="container">
+  {@render children()}
+</main>
+
+<style>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
