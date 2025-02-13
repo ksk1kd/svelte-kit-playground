b17d33a27094396df877cc9677c54afed552ee15
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-14 01:11:08 +0900

    add prerender setting

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,9 +1,12 @@
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
+  console.log('blog list load')
   type Blog = { title: string; id: string }
   const response = await fetch('https://api.vercel.app/blog')
   return {
     posts: (await response.json()) as Blog[],
   }
 }
+
+export const prerender = true
--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ params }) => {
+  console.log('blog detail load')
   type Blog = { title: string; id: string }
   const response = await fetch(`https://api.vercel.app/blog/${params.slug}`)
   if (!response.ok) {
@@ -11,3 +12,5 @@ export const load: PageServerLoad = async ({ params }) => {
     post: (await response.json()) as Blog,
   }
 }
+
+export const prerender = true
